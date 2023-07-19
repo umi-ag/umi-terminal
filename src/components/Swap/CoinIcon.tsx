ad96cdefd031e107481b6ca2a1ee5f7af3f7853e
imataka7 <t.imagawa@kakehashi.life>
2023-07-20 02:45:05 +0900

    Remove comments

--- src/components/Swap/CoinIcon.tsx
+++ src/components/Swap/CoinIcon.tsx
@@ -7,18 +7,4 @@ export const CoinIcon: React.FC<{ iconUrl?: string | null }> = (props) => {
   return (
     <img src={url} alt="coin icon" className="w-8 h-8 rounded-full" />
   );
-  // console.log(props.iconUrl);
-
-  // return (
-  //   <object data={props.iconUrl ?? ''} className="w-8 h-8 rounded-full">
-  //     <img src={coinLogo} alt="coin logo" className="w-8 h-8 rounded-full"/>
-  //   </object>
-  // );
-
-  // return (
-  //   <picture>
-  //     <source srcSet={props.iconUrl ?? ''} />
-  //     <img src={coinLogo} alt="coin logo" className="w-8 h-8 rounded-full" />
-  //   </picture>
-  // );
 };
