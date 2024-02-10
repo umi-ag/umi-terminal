690597ec6ce74852fbf2bdff20c19c78fc61c074
imataka7 <imataka7@gmail.com>
2024-02-10 19:29:12 +0900

    Update dependencies and refactor App component

--- packages/terminal/src/App.tsx
+++ packages/terminal/src/App.tsx
@@ -1,16 +1,18 @@
-import { JsonRpcProvider, mainnetConnection } from '@mysten/sui.js';
+import { SuiClient, getFullnodeUrl } from '@mysten/sui.js/client';
 import { ConnectButton, useWalletKit } from '@mysten/wallet-kit';
 import './App.scss';
 import { UmiTerminal, UmiTerminalModal } from './components/Terminal';
 
 function App() {
   const { currentAccount, currentWallet } = useWalletKit();
-  const provider = new JsonRpcProvider(mainnetConnection);
+  const provider = new SuiClient({
+    url: getFullnodeUrl('mainnet'),
+  });
 
   return (
     <div className="bg-black">
-      <div className="p-16 w-100vw h-100vh">
-        <div className="mx-auto my-0 w-[400px]">
+      <div className="p-8 w-100vw h-100vh">
+        <div className="mx-auto my-0 max-w-[500px]">
           <UmiTerminal
             accountAddress={currentAccount?.address}
             wallet={currentWallet}
